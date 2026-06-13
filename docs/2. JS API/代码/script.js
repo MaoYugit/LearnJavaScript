a90381337eb8d1becb4707fb15067f6d079ff7d3
maoyu <2874553847@qq.com>
2026-06-13 12:39:58 +0800

    docs: 删除不需要的文档

--- docs/2. JS API/代码/script.js
+++
@@ -1,9 +0,0 @@
-btn = document.querySelector("#myButton");
-
-function showAlert() {
-  alert("hello new one!");
-}
-
-btn.addEventListener("click", showAlert);
-
-btn.removeEventListener("click", showAlert);
